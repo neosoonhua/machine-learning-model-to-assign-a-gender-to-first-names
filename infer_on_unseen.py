5dc16401e754014fd2269728f2033337982913ad
neosoonhua <neo_soon_hua@yahoo.com.sg>
2022-07-17 21:43:54 +0800

    Update model name to reflect hyperparameter-tuning

--- infer_on_unseen.py
+++ infer_on_unseen.py
@@ -11,23 +11,22 @@ def text_cleaning_and_split(text):
 
     return text
 
-
 import pandas as pd
 import pickle
-model_filename = "model.pkl"
+model_filename = "model4a1.pkl"
 with open(model_filename, 'rb') as file:
-    vectorizer, X_train, model = pickle.load(file)
+    vectorizer, x_train, best_random = pickle.load(file)
     
 # make predictions
 unseen_names = pd.read_csv("Data/unseen_names.csv")
 # print(unseen_names)
 unseen_names_cleaned = unseen_names['name'].apply(text_cleaning_and_split)
-X_unseen  = vectorizer.transform(unseen_names_cleaned)
-# print(X_unseen)
+x_unseen  = vectorizer.transform(unseen_names_cleaned)
+# print(x_unseen)
 y_unseen = unseen_names['gender']
 
-score_unseen = model.score(X_unseen, y_unseen)
+score_unseen = best_random.score(x_unseen, y_unseen)
 print("Accuracy for unseen set:", score_unseen)
 
-y_pred_unseen = model.predict(X_unseen)
-# print(y_pred_unseen)
+y_pred_unseen = best_random.predict(x_unseen)
+print(y_pred_unseen)
